15947299c3b82015b0c5426b6a2ca749ce91fd61
andres7o7 <andresrevollohuanca@gmail.com>
2023-04-04 01:20:36 -0400

    .

--- Frontend/src/componentes/OfertaNueva.js
+++ Frontend/src/componentes/OfertaNueva.js
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react';
 import swal from "sweetalert";
 import logo from '../imagenes/registroicono.png';
@@ -57,7 +58,7 @@ function RegistrarOfertaNueva() {
           <div className='row align-items-start'>
           <div className='col'>
           <label htmlFor="producto" >Producto*</label>
-          <input list="productos" className="form-control "  id="producto" minlength="1" maxlength="6" required  name="producto" value={producto} onChange={(e) => setProducto(e.target.value)} />
+          <input list="productos" className="form-control "  id="producto" minlength="1" maxlength="20" required  name="producto" value={producto} onChange={(e) => setProducto(e.target.value)} />
           <datalist id="productos">
             <option value="Manzana" />
             <option value="Banana" />
@@ -65,25 +66,25 @@ function RegistrarOfertaNueva() {
             <option value="Mango" />
           </datalist>
           <br />
-          <label htmlFor="precio" >Precio venta*</label>
+          <label htmlFor="precio" >Precio de Venta(bs)*</label>
           <input type='number' className="form-control " id="precio" step="0.01"placeholder='precio*' min="0.5" max="999999"  required value={precio} onChange={(e) => setPrecio(e.target.value)} />
           <br />
           
           </div>
 
           <div className='col'>
-          <label htmlFor="inicio">inicio oferta*</label>
+          <label htmlFor="inicio">Inicio de Oferta*</label>
           <input type="date" className="form-control " name="ini" min={formattedDate} id="inicio" placeholder='fecha-inicio*'required value={inicio} onChange={(e) => setInicio(e.target.value)} />
           <br />
-          <label htmlFor="fin">fin oferta*</label>
-          <input type= "date" className="form-control "  min={formattedDate} id="fin" placeholder='fecha-fin*'required value={fin} onChange={(e) => setFin(e.target.value)} />
+          <label htmlFor="fin">Fin de Oferta*</label>
+          <input type= "date" className="form-control "  min={inicio} id="fin" placeholder='fecha-fin*'required value={fin} onChange={(e) => setFin(e.target.value)} />
           <br />
           
           </div>
           
           </div>
           <br />
-          <label htmlFor="descripcion" className='container text-center'>descripcion*</label>
+          <label htmlFor="descripcion" className='container text-center'>Descripción*</label>
           <input type="text"className="form-control "  id="descripcion" placeholder='descripcion*'minlength="10" maxlength="100"required value={descripcion} onChange={(e) => setDescripcion(e.target.value)} />
           
           <br />
