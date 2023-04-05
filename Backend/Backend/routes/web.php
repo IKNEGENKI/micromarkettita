19f61e9e498d73ec627ce53fc49851ffc6ddf8d4
crexpo20 <crespomauricio16@gmail.com>
2023-04-04 20:53:51 -0400

    arreglo

--- Backend/Backend/routes/web.php
+++ Backend/Backend/routes/web.php
@@ -12,11 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-/*Route::fallback(function() {                
-        return view('index');
-     });
-     */
- /*   
+
+
 Route::get('/', function () {
     return view('welcome');
-});*/
+});
